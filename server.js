dc4a5854d7c6e2e647ff18fbda2e738aad71b5ad
Tieyun <mrrubiks0418@gmail.com>
2022-11-12 19:48:09 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(3000, () => { console.log("Server listening on 3000"); });
+app.listen(process.env.PORT, () => { console.log("Server listening on 3000"); });
 app.get("/", (req, res) => {
     res.sendFile(__dirname + '/bmiCalc.html');
 });
